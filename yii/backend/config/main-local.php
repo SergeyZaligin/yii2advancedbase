cf37d97b21ee0beed526c7452871fed62199db36
cobweb <codename.cobweb@gmail.com>
2018-02-18 15:16:45 +0300

    first commit

---
+++ yii/backend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'WFns-L8_uKcogaIv5oE_6ZbTTv2a16Jo',
+        ],
+    ],
+];
---
+++ yii/frontend/config/main-local.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '9y5sbVAQwv0NR6cZplr7FNruAO0zp5IT',
+        ],
+    ],
+];
